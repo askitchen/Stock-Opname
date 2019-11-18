6a888f349a3dd0e451ed77427f6a6fee2f7964c3
Putu Wirya <tuebom.wirya@gmail.com>
2019-11-18 17:34:19 +0800

    update

--- www/js/routes.js
+++ www/js/routes.js
@@ -8,154 +8,42 @@ routes = [
       // App instance
       var app = router.app;
 
-      // Show Preloader
-      app.preloader.show();
-
-      app.request.getJSON( app.data.endpoint + 'dashboard', function(res) {
-
-        // Hide Preloader
-        app.preloader.hide();
-
-        // console.log(res)
-        // var data = JSON.parse(res)
         
-        // Resolve route to load page
-        resolve(
-          {
-            componentUrl: './pages/home.html',
-          },
-          {
-            context: {
-              banner: res.banner,
-              data: res.categories,
-            }
-          }
-        );
-
-      });
-    },
-  },
-  // {
-  //   path: '/login/',
-  //   componentUrl: './pages/login.html',
-  // },
-  {
-    path: '/stock-opname/',
-    async: function (routeTo, routeFrom, resolve, reject) {
-      // Router instance
-      // var router = this;
-
-      // App instance
-      // var app = router.app;
-
       // Resolve route to load page
       resolve(
         {
-          componentUrl: './pages/stock-opname.html',
+          componentUrl: './pages/home.html',
+        },
+      );
+    },
+  },
+  {
+    path: '/find/:kode',
+    async: function (routeTo, routeFrom, resolve, reject) {
+      // Router instance
+      var router = this;
+
+      // App instance
+      var app = router.app;
+      var kode = routeTo.params.kode;
+
+        
+      // Resolve route to load page
+      resolve(
+        {
+          componentUrl: './pages/home.html',
+        },
+        {
+          context: {
+            kode: kode,
+          }
         }
       );
     },
   },
   {
-    path: '/cek-resi/:nomor',
-    async: function (routeTo, routeFrom, resolve, reject) {
-      // Router instance
-      var router = this;
-
-      // App instance
-      var app = router.app;
-      var order_id = routeTo.params.nomor;
-
-      // Show Preloader
-      app.preloader.show();
-
-      app.request.getJSON( app.data.endpoint + 'resi/'+order_id, function(res) {
-
-        app.preloader.hide();
-
-        resolve (
-          { componentUrl: './pages/cek-resi.html' },
-          { context: { data: res } }
-        );
-      });
-    }
-  },
-  {
-    path: '/settings/',
-    url: './pages/settings.html',
-  },
-  {
-    path: '/account/',
-    async: function (routeTo, routeFrom, resolve, reject) {
-      // Router instance
-      var router = this;
-
-      // App instance
-      var app = router.app;
-
-      if (!app.data.bLogedIn) {
-        
-        app.data.lastURL = '/account/';
-
-        resolve(
-          {
-            componentUrl: './pages/login.html',
-          }
-        );
-        return;
-      }
-      
-      resolve(
-        {
-          componentUrl: './pages/account.html',
-        }
-      );
-      return;
-    }
-  },
-  {
-    path: '/profile/',
-    async: function (routeTo, routeFrom, resolve, reject) {
-      // Router instance
-      var router = this;
-
-      // App instance
-      var app = router.app;
-
-      if (!app.data.bLogedIn) {
-        
-        app.data.lastURL = '/profile/';
-
-        resolve(
-          {
-            componentUrl: './pages/login.html',
-          }
-        );
-        return;
-      }
-
-      // Show Preloader
-      app.preloader.show();
-        
-      app.request.getJSON( app.data.endpoint + 'member/'+app.data.mbrid, function(res) {
-          
-        // var data = JSON.parse(res);
-
-        resolve (
-          { componentUrl: './pages/profile.html' },
-          { context: { data: res } }
-        );
-        app.preloader.hide();
-      });
-    }
-  },
-  {
-    path: '/chat/',
-    componentUrl: './pages/chat.html',
-  },
-  {
-    path: '/notifications/',
-    componentUrl: './pages/notifications.html',
+    path: '/search/',
+    componentUrl: './pages/search.html',
   },
   // Default route (404 page). MUST BE THE LAST
   {
